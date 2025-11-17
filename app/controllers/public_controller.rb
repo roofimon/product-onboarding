1052a62fd89c9cef393321c93b39a45f66ff3fa9
ruufimon <ruuf@odds.team>
2025-11-17 10:22:20 +0700

    Add product search functionality to public page
    - Add search form to public product listing page
    - Implement search by product name and description
    - Add Clear button when search is active
    - Update empty state messages for search results
    - Remove header section from public index page

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,6 +3,15 @@ class PublicController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    
+    if params[:search].present?
+      search_term = "%#{params[:search]}%"
+      @products = @products.where(
+        "name LIKE ? OR description LIKE ?", 
+        search_term, 
+        search_term
+      )
+    end
   end
 
   def show
